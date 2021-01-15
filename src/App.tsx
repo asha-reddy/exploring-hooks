796ae87d8f1656cf252cabe3a3930cd4c3cf4a15
Asha Reddy <asha.reddy@anaplan.com>
2021-01-15 16:19:18 +0000

    clone and stripped initial create-react-app typescipt app

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import React from "react";
+import Board from "./Board";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Board />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Board.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Board() {
+  return <div>HELLO</div>;
+}
+
+export default Board;
