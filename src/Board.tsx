fb1ad72b5c51620f0b58e8aeaef3daef9e594338
Asha Reddy <asha.reddy@anaplan.com>
2021-01-18 01:37:43 +0000

    persist state i.e. likes on refresh

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,11 +1,39 @@
 import React, { useState } from "react";
 
+function saveToLocalStorage(likes: string) {
+  try {
+    const serializedLikes = JSON.stringify(likes);
+    localStorage.setItem("likes", serializedLikes);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+function loadFromLocalStorage() {
+  try {
+    const serializedLikes = localStorage.getItem("likes");
+    if (serializedLikes == null) return undefined;
+    return JSON.parse(serializedLikes);
+  } catch (e) {
+    console.log(e);
+    return undefined;
+  }
+}
+
+function onLike(
+  likes: number,
+  setLikes: React.Dispatch<React.SetStateAction<number>>
+) {
+  setLikes((likes: number) => likes + 1);
+  saveToLocalStorage(JSON.stringify(likes));
+}
+
 function Board() {
   const [likes, setLikes] = useState(0);
   return (
     <div>
-      HELLO, Likes: {likes}
-      <button onClick={() => setLikes((likes) => likes + 1)}>Like</button>
+      HELLO, Page Likes: {loadFromLocalStorage()}
+      <button onClick={() => onLike(likes, setLikes)}>Like</button>
     </div>
   );
 }
